51ba9dea826cdc6678743d7fb3e08e3a8daa508f
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-05-21 03:04:18 +0600

    ui: add success state for download button

--- lib/pages/qr_view/wigets/download_button.dart
+++ lib/pages/qr_view/wigets/download_button.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 import '../../../config/config.dart';
 import '../../../domain/qr_tools/qr_tools.dart' as qr_tools;
 
+const double _minWidth = 50;
+
 class DownloadButton extends StatefulWidget {
   final String qrcodeRawValue;
   final Color fg;
   final Color bg;
   final Function onDownload;
-
   const DownloadButton(
     this.qrcodeRawValue, {
     Key? key,
@@ -22,17 +23,13 @@ class DownloadButton extends StatefulWidget {
 }
 
 class _DownloadButtonState extends State<DownloadButton> {
-  final double _minWidth = 50;
-  bool _isLoading = false;
-  bool _isAnimating = false;
+  bool _isAnimating = true;
+  var _state = _ButtonState.init;
 
-  void _download() {
-    setState(() {
-      _isLoading = true;
-      _isAnimating = true;
-    });
-
-    // Future.delayed(const Duration(seconds: 1), () => setState(() => _isLoading = false));
+  void _download() async {
+    setState(() => _state = _ButtonState.loading);
+    // await Future.delayed(const Duration(seconds: 1), () => setState(() => _state = _ButtonState.success));
+    // await Future.delayed(const Duration(seconds: 1), () => setState(() => _state = _ButtonState.init));
     Future.delayed(const Duration(milliseconds: 700), saveQrAsImg);
   }
 
@@ -43,13 +40,15 @@ class _DownloadButtonState extends State<DownloadButton> {
       bg: widget.bg,
       albumName: appName,
     );
+    setState(() => _state = _ButtonState.success);
     widget.onDownload(success ?? false);
-    setState(() => _isLoading = false);
+    Future.delayed(const Duration(seconds: 2), () => setState(() => _state = _ButtonState.init));
   }
 
   @override
   Widget build(BuildContext context) {
-    bool _isStretched = _isAnimating || !_isLoading;
+    bool isStretched = _isAnimating || _state == _ButtonState.init;
+    bool isDone = _state == _ButtonState.success;
     double width = MediaQuery.of(context).size.width;
 
     return Container(
@@ -59,35 +58,29 @@ class _DownloadButtonState extends State<DownloadButton> {
       padding: EdgeInsets.all(Constants.verticalPadding),
       child: AnimatedContainer(
         duration: const Duration(milliseconds: 220),
-        curve: Curves.easeInOutCirc,
-        width: _isLoading ? _minWidth : width,
-        onEnd: () => setState(() => _isAnimating = false),
-        child: _isStretched ? buildBtn(_download) : buildLoading(),
+        curve: Curves.easeInCirc,
+        width: _state == _ButtonState.init ? width : _minWidth,
+        onEnd: () => setState(() => _isAnimating = !_isAnimating),
+        child: isStretched
+            ? _Btn(onPress: _download)
+            : isDone
+                ? _SmallBtn.success()
+                : _SmallBtn.loading(context),
       ),
     );
   }
+}
 
-  Widget buildLoading() {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.primary,
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-        child: SizedBox(
-          width: 26,
-          height: 26,
-          child: CircularProgressIndicator(color: Theme.of(context).cardColor, strokeWidth: 2.5),
-        ),
-      ),
-    );
-  }
+class _Btn extends StatelessWidget {
+  final VoidCallback onPress;
+  const _Btn({Key? key, required this.onPress}) : super(key: key);
 
-  Widget buildBtn(VoidCallback onPress) {
+  @override
+  Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPress,
       style: ElevatedButton.styleFrom(
-        fixedSize: Size.fromHeight(_minWidth),
+        fixedSize: const Size.fromHeight(_minWidth),
         padding: const EdgeInsets.symmetric(vertical: 12),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(Constants.borderRadius)),
       ),
@@ -102,4 +95,44 @@ class _DownloadButtonState extends State<DownloadButton> {
       ),
     );
   }
+}
+
+class _SmallBtn extends StatelessWidget {
+  final Widget child;
+  final _ButtonState state;
+  const _SmallBtn({Key? key, required this.child, this.state = _ButtonState.loading}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: _minWidth,
+      width: _minWidth,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: state == _ButtonState.loading ? Theme.of(context).colorScheme.primary : Colors.green,
+        shape: BoxShape.circle,
+      ),
+      child: Center(child: child),
+    );
+  }
+
+  factory _SmallBtn.success() {
+    return const _SmallBtn(
+      state: _ButtonState.success,
+      child: Icon(Icons.done, size: 28),
+    );
+  }
+
+  factory _SmallBtn.loading(BuildContext context) {
+    return _SmallBtn(
+      state: _ButtonState.loading,
+      child: CircularProgressIndicator(color: Theme.of(context).cardColor, strokeWidth: 2.5),
+    );
+  }
+}
+
+enum _ButtonState {
+  init,
+  loading,
+  success,
 }
