3e18d0db55a5b6898891fdacd332213204f55058
Hasan Mahmud <38243998+hasansujon786@users.noreply.github.com>
2022-05-29 23:43:01 +0600

    update: bump version to 1.0.1   fix: Correct theme background color

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -7,7 +7,6 @@ class Constants {
 }
 
 const appName = 'Smart QR';
-const appVersion = '1.0.0';
-const appVersionShort = '1';
+const appVersion = '1.0.1';
 const playStoreAppLink = 'https://play.google.com/store/apps/details?id=com.thinknxtmedia.smart_qr';
 const playStoreMoreAppsLink = 'https://play.google.com/store/apps/developer?id=Think+nXt+Media';
